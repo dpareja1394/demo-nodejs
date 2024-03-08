9521b6ee67d78faf23dab4ac85b7fa9358edb898
Daniel Pareja Londoño <dpareja1394@gmail.com>
2024-03-08 14:43:26 -0500

    Module exports

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log('Hola')
-console.info('Hola info')
+//console.log('Hola')
+//console.info('Hola info')
+
+//globalThis variable local en toda nuestra aplicación. Podemos acceder 
+console.log(globalThis)
+globalThis.console.log('hola')
+
+// Patrón de diseño módulo
+//CommonJS Module
+const { sum } = require('./sum')
+
+globalThis.console.log('suma ', sum(23, 5))
---
+++ sum.js
@@ -0,0 +1,9 @@
+function sum (a, b) {
+    return a+b
+}
+
+// Patrón de diseño módulo
+// CommonJS Module
+module.exports = {
+    sum
+}
