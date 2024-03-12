d6bca65e6c98cd3dd3d0593f4563379797bf3167
Daniel Pareja Londoño <dpareja1394@gmail.com>
2024-03-12 10:46:06 -0500

    CommonJS y Modules

--- 1.os-info.js
+++ 1.os-info.js
@@ -8,5 +8,4 @@ console.log('Arquitectura del sistema operativo', os.arch())
 console.log('CPUs', os.cpus())
 console.log('Memoria libre', os.freemem() / 1024 / 1024)
 console.log('Memoria total', os.totalmem() / 1024 / 1024)
-
-
+console.log('uptime', os.uptime() / 60 / 60)
